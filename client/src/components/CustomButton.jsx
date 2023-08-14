840e552ac661a454cdce4a603dda2f67d20df3e9
Saul Paredes <saulkurosaki13@gmail.com>
2023-08-14 15:15:07 -0600

    CustomButton.jsx Complete Component Done

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import state from '../store';
 import { snapshot } from 'valtio';
+import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ title, type, customStyles, handleClick }) => {
 
@@ -11,9 +12,15 @@ const CustomButton = ({ title, type, customStyles, handleClick }) => {
         if(type === 'filled'){
             return {
                 backgroundColor: snap.color,
-                color: '#fff',
+                color: getContrastingColor(snap.color),
 
             };
+        }else if(type === 'outline'){
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color
+            }
         };
     };
 
